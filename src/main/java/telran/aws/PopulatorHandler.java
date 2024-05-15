8b45c8decfbda6fff4ddb98a6ec83eb6c6ca9403
AlGuron <blizgur8@gmail.com>
2024-05-15 17:27:47 +0300

    ref

--- src/main/java/telran/aws/PopulatorHandler.java
+++ src/main/java/telran/aws/PopulatorHandler.java
@@ -6,7 +6,7 @@ import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.json.simple.JSONObject;
+
 import org.json.simple.parser.ContainerFactory;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -19,40 +19,47 @@ import com.amazonaws.services.dynamodbv2.document.Item;
 import com.amazonaws.services.dynamodbv2.document.Table;
 import com.amazonaws.services.dynamodbv2.document.spec.PutItemSpec;
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.KinesisEvent;
+import com.amazonaws.services.lambda.runtime.events.KinesisEvent.KinesisEventRecord;
 
 public class PopulatorHandler implements RequestHandler<KinesisEvent, String> {
-	private static final PopulatorHandler INSTANCE = new PopulatorHandler();
 	Pattern p = Pattern.compile("{.*}");
+	LambdaLogger log;
 	AmazonDynamoDB client;
 	DynamoDB dynamo;
 	Table table;
-	String tableName = "avg_values";
+	String tableName;
 
 	private PopulatorHandler() {
+		tableName = System.getenv("TABLE_NAME");
+		setDynamoDB();
+	}
+
+	private void setDynamoDB() {
 		client = AmazonDynamoDBClientBuilder.standard().withRegion(Regions.US_EAST_1).build();
 		dynamo = new DynamoDB(client);
 		table = dynamo.getTable(tableName);
 	}
 
-	public static PopulatorHandler getInstance() {
-		return INSTANCE;
-	}
-
 	@Override
 	public String handleRequest(KinesisEvent input, Context context) {
-		input.getRecords().stream().map(r -> new String(r.getKinesis().getData().array())).map(s -> getMapFromString(s))
-				.forEach(s -> putToTable(s));
+		log = context.getLogger();
+		log.log("tableName is " + tableName);
+		input.getRecords().stream().map(this::getJsonfromString).forEach(s -> putToTable(s));
 		return null;
 	}
 
-	private void putToTable(Map<String, Object> map) {
+	private void putToTable(String recordJson) {
+		log.log(String.format("received probeData: %s", recordJson));
+		Map<String, Object> map = getMap(recordJson);
 		table.putItem(new PutItemSpec().withItem(Item.fromMap(map)));
+		log.log(String.format("item %s has been saved to Database", map));
 	}
 
 	@SuppressWarnings("unchecked")
-	private Map<String, Object> getMapFromString(String s) {
+	private Map<String, Object> getMap(String json) {
 		Map<String, Object> res = null;
 		JSONParser parser = new JSONParser();
 
@@ -70,16 +77,18 @@ public class PopulatorHandler implements RequestHandler<KinesisEvent, String> {
 
 		};
 		try {
-			res = (Map<String, Object>) parser.parse(getJsonfromString(s), containerFactory);
+			res = (Map<String, Object>) parser.parse(json, containerFactory);
 		} catch (ParseException e) {
-			System.out.println("position: " + e.getPosition());
-			System.out.println(e);
+			log.log("ERROR JSON parsing: " + "position: " + e.getPosition() + e);
 		}
+		log.log("Map is: " + res);
 		return res;
 	}
 
-	private String getJsonfromString(String s) {
-		Matcher m = p.matcher("s");
+	private String getJsonfromString(KinesisEventRecord event) {
+		String record = new String(event.getKinesis().getData().array());
+		log.log("received string:  " + record);
+		Matcher m = p.matcher(record);
 		m.find();
 		return m.group();
 	}
